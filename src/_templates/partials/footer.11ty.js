96991ccddd5cdd87cf5904a3fcf4ca8f441b18a1
RichardReh <richardreh@gmx.de>
2022-07-12 18:49:41 +0200

    Aufgabe 5 versucht + Aufgabe 6 gemacht

--- src/_templates/partials/footer.11ty.js
+++
@@ -1,5 +0,0 @@
-exports.html = `
-<footer class="footer" id="footer">
-  &copy; 2022 // Cranach Digital Archive
-</footer>
-`;
--- src/_templates/partials/form.11ty.js
+++
@@ -1,7 +0,0 @@
-exports.html = `
-<div>
-  <h3>Metadaten zur Abbildung</h3>
-  <form>
-  </form>
-</div>
-`;
